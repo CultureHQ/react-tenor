eecdb554e435bb6a6b242d7417153b45f06a6b51
Kevin Deisz <kevin.deisz@gmail.com>
2018-07-18 10:37:47 -0400

    Just use babel

---
+++ CHANGELOG.md
@@ -0,0 +1,15 @@
+# Changelog
+
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/) and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
+
+## [Unreleased]
+
+## [0.2.0] - 2018-07-18
+### Changed
+- Don't build the final distribution with `webpack`, just use `babel`.
+- Rename `example` to `docs` so we can publish to github pages.
+
+[Unreleased]: https://github.com/CultureHQ/components/compare/v0.2.0...HEAD
+[0.0.2]: https://github.com/CultureHQ/components/compare/v0.1.0...v0.2.0
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,13 @@
 /* eslint import/no-extraneous-dependencies: off */
-const nodeExternals = require("webpack-node-externals");
+const path = require("path");
+const webpack = require("webpack");
 
 module.exports = {
   output: {
-    libraryTarget: "umd"
+    path: path.join(__dirname, "docs"),
+    filename: "index.js"
   },
+  entry: path.join(__dirname, "docs", "app.js"),
   module: {
     rules: [
       { test: /\.js$/, use: "babel-loader", exclude: /node_modules/ },
@@ -15,7 +18,10 @@ module.exports = {
       }
     ]
   },
-  target: "node",
-  mode: "production",
-  externals: [nodeExternals()]
+  devServer: {
+    contentBase: path.join(__dirname, "docs")
+  },
+  plugins: [
+    new webpack.EnvironmentPlugin(["TOKEN"])
+  ]
 };
