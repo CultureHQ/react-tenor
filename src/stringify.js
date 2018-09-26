57842eb0229e62ebb6ecad155ab56e6b15b771cc
Kevin Deisz <kevin.deisz@gmail.com>
2018-09-26 12:34:35 -0400

    Consolidate stringify function

--- src/stringify.js
+++
@@ -1,7 +0,0 @@
-const stringifyQuery = query => (
-  Object.keys(query).reduce((accum, key, index) => (
-    `${accum}${index === 0 ? "" : "&"}${key}=${query[key]}`
-  ), "?")
-);
-
-export default stringifyQuery;
