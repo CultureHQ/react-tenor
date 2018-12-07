7e97f64af3ae45cfcf2c3a7a0746391d6e71611c
Kevin Deisz <kevin.deisz@gmail.com>
2018-12-06 21:21:01 -0500

    Standardize docs site

--- docs/app.js
+++ docs/app.js
@@ -8,15 +8,30 @@ const App = () => {
   const [selected, setSelected] = useState(null);
 
   return (
-    <main className="container">
-      <h1>
-        <a href="https://github.com/CultureHQ/react-tenor">react-tenor</a>
-      </h1>
-      <div className="selected">
-        {selected && <img src={selected.media[0].tinygif.url} alt="Selected GIF" />}
-      </div>
-      <Tenor onSelect={setSelected} />
-    </main>
+    <>
+      <nav>react-tenor</nav>
+      <main>
+        <h1>
+          <a href="https://github.com/CultureHQ/react-tenor">react-tenor</a>
+        </h1>
+        <div className="selected">
+          {selected && <img src={selected.media[0].tinygif.url} alt="Selected GIF" />}
+        </div>
+        <Tenor onSelect={setSelected} />
+      </main>
+      {ReactDOM.createPortal(
+        <footer>
+          <p>
+            Copyright (c) 2018 CultureHQ
+            <br />
+            <a href="https://github.com/CultureHQ/react-tenor">
+              github.com/CultureHQ/react-tenor
+            </a>
+          </p>
+        </footer>,
+        document.body
+      )}
+    </>
   );
 };
 
